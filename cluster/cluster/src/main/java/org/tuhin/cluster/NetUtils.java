85cb8cc10228f7ef8df33a8ddbe4550cccabf5fc
tuhinp2m <tuhin.sengupta@gmail.com>
2019-07-06 21:41:03 +0530

    Added NetUtils

---
+++ cluster/cluster/src/main/java/org/tuhin/cluster/NetUtils.java
@@ -0,0 +1,37 @@
+package org.tuhin.cluster;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class NetUtils {
+
+    public static void setInterface(MulticastSocket multicastSocket, boolean preferIpv6) throws IOException {
+        boolean interfaceSet = false;
+        Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface i = (NetworkInterface) interfaces.nextElement();
+            Enumeration addresses = i.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress address = (InetAddress) addresses.nextElement();
+                if (preferIpv6 && address instanceof Inet6Address) {
+                    multicastSocket.setInterface(address);
+                    interfaceSet = true;
+                    break;
+                } else if (!preferIpv6 && address instanceof Inet4Address) {
+                    multicastSocket.setInterface(address);
+                    interfaceSet = true;
+                    break;
+                }
+            }
+            if (interfaceSet) {
+                break;
+            }
+        }
+    }
+
+}
